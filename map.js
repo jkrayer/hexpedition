8f7058a32f7ac555a6d222930ea61f3a49a016da
Jim Krayer <jameskrayer@yahoo.com>
2024-02-10 20:47:21 -0500

    Very basic rendering

---
+++ map.js
@@ -0,0 +1,210 @@
+/**
+ * @typedef { "capital" | "town" | "village" | "fort" | "ruin" | "lair" | "ruin-lair" | "forest" | "plains" | "swamp" | "hills" | "mountain" | "forest-hills" | "plains-hills" } Terrain
+ */
+
+/**
+ * @typedef { Object } HEX
+ * @property { number } id - The default number of players.
+ * @property { boolean } revealed - Hex has been viewed
+ * @property { boolean } traversed - Hex has been travelled
+ * @property { Terrain } revealedTerrain - Hex to show when revealed is true
+ * @property { Terrain } traversedTerrain - Hex to show when traversed is true
+ */
+
+/**
+ * @type {Array<HEX>}
+ */
+const hex = [
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "forest",
+    traversedTerrain: "forest",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+  {
+    revealed: false,
+    traversed: false,
+    revealedTerrain: "swamp",
+    traversedTerrain: "swamp",
+  },
+];
