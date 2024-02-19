f85e885dfd7ce2a390595cc3c31e8149da911cbc
Jim Krayer <jameskrayer@yahoo.com>
2024-02-19 13:20:12 -0500

    Add state updates to render.

--- scripts/components/HexMap.js
+++ scripts/components/HexMap.js
@@ -3,21 +3,66 @@ import { Observer } from "../lib/Observer.js";
 export default class HexMap extends Observer {
   constructor(appState) {
     super();
-    this.appState = appState;
-    this.render(appState.getHexMap());
+    this.hexMap = appState.getHexMap();
+    this.render();
   }
 
-  render(hexMap) {
-    if (hexMap.loading) {
+  terrain(hex) {
+    hex.traversed
+      ? hex.traversedTerrain
+      : hex.revealed
+      ? hex.revealedTerrain
+      : "unexplored";
+  }
+
+  renderHex(hex, index) {
+    const className = hex.occupied
+      ? "start"
+      : hex.adjacent
+      ? "adjacent"
+      : "unexplored"; // exists so classList.add doesn't error
+    const data = this.terrain(hex);
+
+    const d = document.createElement("div");
+    d.setAttribute("data-key", index);
+    d.classList.add(className);
+    d.append(`${data}-${index}`);
+
+    return d;
+  }
+
+  renderAll() {
+    const mapEl = document.getElementById("map"); // niavely not dealing with null
+    const { map_data: mapData, map_height: mapHeight } = this.hexMap.data;
+
+    for (let i = 0, ii = mapData.length; i < ii; i) {
+      let d = document.createElement("div");
+
+      let j = i;
+      let jj = Math.min(i + mapHeight, mapData.length);
+
+      for (; j < jj; j++) {
+        d.appendChild(this.renderHex(mapData[j], j));
+      }
+
+      i = j;
+      mapEl.appendChild(d);
+    }
+  }
+
+  render() {
+    console.log("render", this.hexMap);
+    if (this.hexMap.loading) {
       console.log("Loading...");
-    } else if (hexMap.error !== null) {
-      console.log("Has error:", error);
+    } else if (this.hexMap.error !== null) {
+      console.log("Has error:", this.hexMap.error);
     } else {
-      console.log("Will render:", hexMap);
+      this.renderAll();
     }
   }
 
   update(state) {
-    this.render(state.hexmap);
+    this.hexMap = state.hexmap;
+    this.render();
   }
 }
