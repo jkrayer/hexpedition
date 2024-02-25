8db832faa66d3af67bcef9e6a6a1a4067e1dd960
Jim Krayer <jameskrayer@yahoo.com>
2024-02-25 15:45:19 -0500

    Move ui from root

---
+++ ui/src/components/Controls.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Controls() {
+  return (
+    <div id="controls">
+      <button type="button">(un)Lock</button>
+      <button type="button" disabled>
+        Scout
+      </button>
+    </div>
+  );
+}
